1e12081f3a41e001f30b75dff1c88acfab8baa5d
Nadira Farhat <nadirafarhat770@gmail.com>
2024-11-22 11:04:28 +0530

    init

---
+++ src/songs/songs.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
+
+@Controller('songs')
+export class SongsController {
+    constructor(private songsService: SongsService){}
+
+    @Post()
+    create(){
+        return this.songsService.create('Animals by Martin Garrix');
+    }
+
+    @Get()
+    findAll(){
+        return this.songsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(){
+        return "fetch songs based on id"
+    }
+
+    @Put(':id')
+    update(){
+        return "update song on the based on id"
+    }
+
+    @Delete(':id')
+    delete(){
+        return "delete song based on id"
+    }
+
+}
